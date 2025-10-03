6c50963238f2be6111ead674facb779277c350f9
Willdofitness <will-bright@hotmail.com>
2025-10-03 19:43:50 +1300

    Add files via upload

---
+++ reaction_game_pwa/sw.js
@@ -0,0 +1,38 @@
+
+const CACHE_NAME = 'reaction-game-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  // CDN assets (will cache after first load)
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => Promise.resolve())
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request).then((resp) => {
+        const respClone = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, respClone)).catch(()=>{});
+        return resp;
+      }).catch(() => cached || Response.error());
+    })
+  );
+});
